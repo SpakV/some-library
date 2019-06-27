add0b73bdf54ebe454849496bf2c017ee34195ba
sergey <paksergey94@gmail.com>
2019-06-28 09:13:45 +1000

    init

---
+++ src/SomeLibrary.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+/**
+ * Some library.
+ */
+class SomeLibrary {
+	/**
+	 * Some method.
+	 *
+	 * @return bool
+	 */
+	public static function method(): bool {
+		return true;
+	}
+}
